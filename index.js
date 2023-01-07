aba5327f18f84a136daaae79a993e7dd234feb5d
durotimicodes <edmondfagbuyi@gmail.com>
2023-01-07 02:28:21 +0000

    adding a read me file

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+let num = 21
+let firstName = 'Oluwadurotimi'
+let middleName = 'Edmond'
+let lastName = 'Fagbuyi'
+const templLiteral = `My new age is ${21}, but my names remain ${firstName} ${middleName} ${lastName}`
+
+console.log(templLiteral)
