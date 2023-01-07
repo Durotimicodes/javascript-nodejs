93770ba9c45c6f6a60177d1c6610e8df0c31b0c3
durotimicodes <edmondfagbuyi@gmail.com>
2023-01-07 04:21:19 +0000

    statments and expressions

--- index.js
+++ index.js
@@ -7,3 +7,20 @@ let lastName = 'Fagbuyi'
 const templLiteral = `My new age is ${21}, but my names remain ${firstName} ${middleName} ${lastName}`
 
 console.log(templLiteral)
+
+
+//switch statement
+
+const day = 'Tuesday'
+
+switch(day) {
+    case 'Monday': 
+    case 'Tuesday':
+    case 'Wednesday':
+    case 'Thursday':
+    console.log(`Today is ${day}`)
+    break
+
+    default:
+    console.log(`Todat is unknown`)
+}
